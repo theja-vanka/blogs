431022442ffa40e0689a7d83658783917e5df6af
Krishnatheja Vanka <theja.vanka@gmail.com>
2026-07-14 13:22:21 +0530

    Deploy 4588bc0 — 2026-07-14

--- _next/static/chunks/app/about/page-5273946b01a70bea.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7180,7220,8806],{3581:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,52619,23)),Promise.resolve().then(n.t.bind(n,81356,23))}},e=>{e.O(0,[2619,1356,8441,1255,7358],()=>e(e.s=3581)),_N_E=e.O()}]);
